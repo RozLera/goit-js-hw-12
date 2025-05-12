75064a2a03e33f42446402bc9223622102ef4ffe
RozLera <konobor@gmail.com>
2025-05-12 16:53:49 +0000

    Deploying to gh-pages from @ RozLera/goit-js-hw-12@e19fc9e64712e2ede098df546b06fbf2dc8d771f 🚀

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import{a as u,S as p,i as l}from"./assets/vendor-CocXUmuy.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&a(o)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();u.defaults.baseURL="https://pixabay.com/api/";function g(s){return u("",{params:{q:s,key:"50191530-19c60f56ef7d22126639596e8",image_type:"photo",orientation:"horizontal",safesearch:!0}}).then(i=>i.data).catch(i=>i)}const h=new p(".js-gallery .image-link",{captionsData:"alt",captionDelay:250}),f=document.querySelector(".js-gallery");function y(s){f.innerHTML=s.map(({tags:i,largeImageURL:r,webformatURL:a,likes:e,views:t,comments:o,downloads:m})=>`<li class="gallery-item">
+import{a as m,S as q,i as n}from"./assets/vendor-CocXUmuy.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const s of r.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&o(s)}).observe(document,{childList:!0,subtree:!0});function a(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(e){if(e.ep)return;e.ep=!0;const r=a(e);fetch(e.href,r)}})();m.defaults.baseURL="https://pixabay.com/api/";async function y(t,i){try{return(await m("",{params:{q:t,key:"50191530-19c60f56ef7d22126639596e8",image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:15,page:i}})).data}catch{}}const g=new q(".js-gallery .image-link",{captionsData:"alt",captionDelay:250}),h=document.querySelector(".js-gallery");function v(t){const i=t.map(({tags:a,largeImageURL:o,webformatURL:e,likes:r,views:s,comments:b,downloads:R})=>`<li class="gallery-item">
                 <div>
-                  <a href="${r}" class="image-link">
+                  <a href="${o}" class="image-link">
                     <img
-                      src="${a}"
-                      alt="${i}"
+                      src="${e}"
+                      alt="${a}"
                       class="gallery-image"
                     />
                   </a>
@@ -12,21 +12,21 @@ import{a as u,S as p,i as l}from"./assets/vendor-CocXUmuy.js";(function(){const 
                   <ul class="image-info">
                     <li class="info-item">
                       <p class="info-value-tittle">Likes</p>
-                      <p class="info-value" data-likes>${e}</p>
+                      <p class="info-value" data-likes>${r}</p>
                     </li>
                     <li class="info-item">
                       <p class="info-value-tittle">Views</p>
-                      <p class="info-value" data-views>${t}</p>
+                      <p class="info-value" data-views>${s}</p>
                     </li>
                     <li class="info-item">
                       <p class="info-value-tittle">Comments</p>
-                      <p class="info-value" data-comments>${o}</p>
+                      <p class="info-value" data-comments>${b}</p>
                     </li>
                     <li class="info-item">
                       <p class="info-value-tittle">Downloads</p>
-                      <p class="info-value" data-downloads>${m}</p>
+                      <p class="info-value" data-downloads>${R}</p>
                     </li>
                   </ul>
                 </div>
-              </li>`).join("")}function n(){f.innerHTML=""}function v(s){s.classList.remove("hidden")}function L(s){s.classList.add("hidden")}const d=document.querySelector(".js-form"),c=document.querySelector(".js-loader");d.addEventListener("submit",b);function b(s){s.preventDefault();const i=s.target.elements["search-text"].value.trim();if(i===""){l.warning({position:"topRight",message:"Please enter the correct query!"});return}n(),v(c),g(i).then(r=>{if(r.hits.length===0){l.warning({position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"}),n();return}y(r.hits),h.refresh()}).catch(r=>{l.error({position:"topRight",message:`ERROR: ${r}`})}).finally(()=>{L(c),d.reset()})}
+              </li>`).join("");h.insertAdjacentHTML("beforeend",i)}function d(){h.innerHTML=""}function f(t){t.classList.remove("hidden")}function L(t){t.classList.add("hidden")}let l="",p=1;const u=document.querySelector(".js-form"),c=document.querySelector(".js-loader"),w=document.querySelector(".btn-loader");u.addEventListener("submit",S);w.addEventListener("click",$);async function S(t){if(t.preventDefault(),l=t.target.elements["search-text"].value.trim(),l===""){n.warning({position:"topRight",message:"Please enter the correct query!"});return}d(),f(c);try{const i=await y(l);if(i.hits.length===0){n.warning({position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"}),d();return}i.totalHits>15&&f(w),v(i.hits),g.refresh()}catch(i){n.error({position:"topRight",message:`ERROR: ${i}`})}finally{L(c),u.reset()}}async function $(){try{p+=1;const t=await y(l,p);v(t.hits),g.refresh()}catch(t){n.error({position:"topRight",message:`ERROR: ${t}`})}finally{L(c),u.reset()}}
 //# sourceMappingURL=index.js.map
